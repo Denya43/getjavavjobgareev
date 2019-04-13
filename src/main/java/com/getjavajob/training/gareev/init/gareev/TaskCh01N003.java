b89a8b386703d749a34dc6e1a21da8ca5b8a1656
Denis <gdenia1@Gmail.com>
2019-04-13 19:50:51 +0300

    First commit

---
+++ src/main/java/com/getjavajob/training/gareev/init/gareev/TaskCh01N003.java
@@ -0,0 +1,26 @@
+package main.java.com.getjavajob.training.gareev.init.gareev;
+
+import java.util.Scanner;
+
+/**
+ * Created by Denis on 13.04.2019.
+ */
+public class TaskCh01N003 {
+    public static void main(String[] args){
+        System.out.println("Enter type of figure \n" +
+                "   1 - Cone\n" +
+                "   2 - Pyramid\n" +
+                "   3 - Cylinder\n" +
+                "   4 - Prism");
+        Scanner sc = new Scanner(System.in);
+        int numFigure = sc.nextInt();
+        /*switch(numFigure){
+            case 1: int baseRadius1 = sc.nextInt(); int coneHeight1 = sc.nextInt();break;
+            case 2: int numOfCorn = sc.nextInt(); int lengthOfEdges2 = sc.nextInt(); int pyramidHeight = sc.nextInt();break;
+            case 3:  int baseRadius3 = sc.nextInt(); int cylinderHeight = sc.nextInt();break;
+            case 4: int numOfAngle= sc.nextInt();int lengthOfEdges4 =sc.nextInt();int prismHeight = sc.nextInt();break;
+        }*/
+
+
+    }
+}
